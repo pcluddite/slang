22634bc49a26621c561f386dd65ce590fc4773c7
Tim Baxendale <pcluddite@hotmail.com>
2019-06-22 14:14:44 -0500

    Normalized other scanners

--- tbasic/Lexer/CScanner/CScanner.cs
+++ tbasic/Lexer/CScanner/CScanner.cs
@@ -8,36 +8,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Tbasic.Components;
 
 namespace Tbasic.Lexer
 {
     internal class CScanner : DefaultScanner
     {
-        public override bool NextVariable(out IEnumerable<char> name, out IList<IEnumerable<char>> indices)
+        public CScanner(StringStream stream) : base(stream)
         {
-            if (NextValidIdentifier(out name)) {
-                NextIndices(out indices);
-                return true;
-            }
-            else {
-                name = null; indices = null;
-                return false;
-            }
-        }
-
-        public override bool NextIndices(out IList<IEnumerable<char>> indices)
-        {
-            var lazy_indices = new Lazy<List<IEnumerable<char>>>();
-            int start = FindNonWhiteSpace();
-
-            while (CharAt(start) == '[') {
-                SkipGroup();
-                lazy_indices.Value.Add(StringSegment.Create(InternalBuffer, start, Position - start));
-                start = FindNonWhiteSpace();
-            }
-
-            indices = lazy_indices.IsValueCreated ? lazy_indices.Value : null;
-            return (indices != null);
         }
     }
 }
--- tbasic/Lexer/TerminalScanner/TerminalScanner.cs
+++ tbasic/Lexer/TerminalScanner/TerminalScanner.cs
@@ -14,37 +14,5 @@ namespace Tbasic.Lexer
             : base(buffer)
         {
         }
-
-        protected override char EscapeCharacter
-        {
-            get {
-                return '`'; // use the backtick as an escape character. this way we don't have to constantly use \\ in paths.
-            }
-        }
-
-        public override IScanner Scan(IEnumerable<char> buffer)
-        {
-            return new TerminalScanner(buffer.ToString());
-        }
-
-        public override bool NextFunction(out IEnumerable<char> name, out IList<IEnumerable<char>> args)
-        {
-            int start = Position;
-            if (base.NextFunction(out name, out args)) {
-                return true;
-            }
-            else if (NextValidIdentifier(out name)) {
-                args = new List<IEnumerable<char>>();
-                IEnumerable<char> token;
-                while (NextWordOrString(out token)) {
-                    args.Add(token);
-                }
-                return true;
-            }
-            else {
-                Position = start;
-                return false;
-            }
-        }
     }
 }

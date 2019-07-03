f0a878311417410309cbeb593fe3b1526470ed70
Tim Baxendale <pcluddite@outlook.com>
2019-07-02 23:01:52 -0500

    Implementing UnaryOp factories

--- slang/Lexer/Tokens/Operators/UnaryOpTokens.Generated.cs
+++ slang/Lexer/Tokens/Operators/UnaryOpTokens.Generated.cs
@@ -12,321 +12,66 @@ namespace Slang.Lexer.Tokens
     /// <summary>
     /// Implementation of the NewOperator
     /// </summary>
-    public partial struct NewOperator : IUnaryOperator
+    public partial class NewOperator : UnaryOperatorFactory
     {
         /// <summary>
         /// Gets the string representation of the operator
         /// </summary>
-        public IEnumerable<char> Text => OperatorString;
+        public override string OperatorString => "NEW";
 
-        /// <summary>
-        /// Throws a NotImplementedException
-        /// </summary>
-        public object Native => throw new NotImplementedException();
-
-        /// <summary>
-        /// Gets the string representation of the operator
-        /// </summary>
-        public string OperatorString => "NEW";
-
-        public bool EvaluateOperand => false;
-
-        /// <summary>
-        /// Determines if this operator is equal to another
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(IUnaryOperator other)
-        {
-            return OperatorString == other.OperatorString && EvaluateOperand == other.EvaluateOperand;
-        }
-
-        /// <summary>
-        /// Determines if this object is equal to another
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            NewOperator? op = obj as NewOperator?;
-            if (op != null)
-                return Equals(op.Value);
-            return false;
-        }
-
-        /// <summary>
-        /// Gets the hash code for this object
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return OperatorString.GetHashCode() ^ EvaluateOperand.GetHashCode();
-        }
-
-        /// <summary>
-        /// Converts this operator to a string
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return OperatorString;
-        }
+        public override bool EvaluateOperand => false;
     }
 
     /// <summary>
     /// Implementation of the PositiveOperator
     /// </summary>
-    public partial struct PositiveOperator : IUnaryOperator
+    public partial class PositiveOperator : UnaryOperatorFactory
     {
         /// <summary>
         /// Gets the string representation of the operator
         /// </summary>
-        public IEnumerable<char> Text => OperatorString;
+        public override string OperatorString => "+";
 
-        /// <summary>
-        /// Throws a NotImplementedException
-        /// </summary>
-        public object Native => throw new NotImplementedException();
-
-        /// <summary>
-        /// Gets the string representation of the operator
-        /// </summary>
-        public string OperatorString => "+";
-
-        public bool EvaluateOperand => true;
-
-        /// <summary>
-        /// Determines if this operator is equal to another
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(IUnaryOperator other)
-        {
-            return OperatorString == other.OperatorString && EvaluateOperand == other.EvaluateOperand;
-        }
-
-        /// <summary>
-        /// Determines if this object is equal to another
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            PositiveOperator? op = obj as PositiveOperator?;
-            if (op != null)
-                return Equals(op.Value);
-            return false;
-        }
-
-        /// <summary>
-        /// Gets the hash code for this object
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return OperatorString.GetHashCode() ^ EvaluateOperand.GetHashCode();
-        }
-
-        /// <summary>
-        /// Converts this operator to a string
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return OperatorString;
-        }
+        public override bool EvaluateOperand => true;
     }
 
     /// <summary>
     /// Implementation of the NegativeOperator
     /// </summary>
-    public partial struct NegativeOperator : IUnaryOperator
+    public partial class NegativeOperator : UnaryOperatorFactory
     {
         /// <summary>
         /// Gets the string representation of the operator
         /// </summary>
-        public IEnumerable<char> Text => OperatorString;
+        public override string OperatorString => "+";
 
-        /// <summary>
-        /// Throws a NotImplementedException
-        /// </summary>
-        public object Native => throw new NotImplementedException();
-
-        /// <summary>
-        /// Gets the string representation of the operator
-        /// </summary>
-        public string OperatorString => "+";
-
-        public bool EvaluateOperand => true;
-
-        /// <summary>
-        /// Determines if this operator is equal to another
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(IUnaryOperator other)
-        {
-            return OperatorString == other.OperatorString && EvaluateOperand == other.EvaluateOperand;
-        }
-
-        /// <summary>
-        /// Determines if this object is equal to another
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            NegativeOperator? op = obj as NegativeOperator?;
-            if (op != null)
-                return Equals(op.Value);
-            return false;
-        }
-
-        /// <summary>
-        /// Gets the hash code for this object
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return OperatorString.GetHashCode() ^ EvaluateOperand.GetHashCode();
-        }
-
-        /// <summary>
-        /// Converts this operator to a string
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return OperatorString;
-        }
+        public override bool EvaluateOperand => true;
     }
 
     /// <summary>
     /// Implementation of the NotOperator
     /// </summary>
-    public partial struct NotOperator : IUnaryOperator
+    public partial class NotOperator : UnaryOperatorFactory
     {
         /// <summary>
         /// Gets the string representation of the operator
         /// </summary>
-        public IEnumerable<char> Text => OperatorString;
+        public override string OperatorString => "NOT";
 
-        /// <summary>
-        /// Throws a NotImplementedException
-        /// </summary>
-        public object Native => throw new NotImplementedException();
-
-        /// <summary>
-        /// Gets the string representation of the operator
-        /// </summary>
-        public string OperatorString => "NOT";
-
-        public bool EvaluateOperand => true;
-
-        /// <summary>
-        /// Determines if this operator is equal to another
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(IUnaryOperator other)
-        {
-            return OperatorString == other.OperatorString && EvaluateOperand == other.EvaluateOperand;
-        }
-
-        /// <summary>
-        /// Determines if this object is equal to another
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            NotOperator? op = obj as NotOperator?;
-            if (op != null)
-                return Equals(op.Value);
-            return false;
-        }
-
-        /// <summary>
-        /// Gets the hash code for this object
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return OperatorString.GetHashCode() ^ EvaluateOperand.GetHashCode();
-        }
-
-        /// <summary>
-        /// Converts this operator to a string
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return OperatorString;
-        }
+        public override bool EvaluateOperand => true;
     }
 
     /// <summary>
     /// Implementation of the BitNotOperator
     /// </summary>
-    public partial struct BitNotOperator : IUnaryOperator
+    public partial class BitNotOperator : UnaryOperatorFactory
     {
         /// <summary>
         /// Gets the string representation of the operator
         /// </summary>
-        public IEnumerable<char> Text => OperatorString;
+        public override string OperatorString => "~";
 
-        /// <summary>
-        /// Throws a NotImplementedException
-        /// </summary>
-        public object Native => throw new NotImplementedException();
-
-        /// <summary>
-        /// Gets the string representation of the operator
-        /// </summary>
-        public string OperatorString => "~";
-
-        public bool EvaluateOperand => true;
-
-        /// <summary>
-        /// Determines if this operator is equal to another
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(IUnaryOperator other)
-        {
-            return OperatorString == other.OperatorString && EvaluateOperand == other.EvaluateOperand;
-        }
-
-        /// <summary>
-        /// Determines if this object is equal to another
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            BitNotOperator? op = obj as BitNotOperator?;
-            if (op != null)
-                return Equals(op.Value);
-            return false;
-        }
-
-        /// <summary>
-        /// Gets the hash code for this object
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            return OperatorString.GetHashCode() ^ EvaluateOperand.GetHashCode();
-        }
-
-        /// <summary>
-        /// Converts this operator to a string
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return OperatorString;
-        }
+        public override bool EvaluateOperand => true;
     }
 }
 
--- slang/Lexer/Tokens/Operators/UnaryOperatorFactory.cs
+++ slang/Lexer/Tokens/Operators/UnaryOperatorFactory.cs
@@ -4,6 +4,7 @@
  *
  *  +++====+++
 **/
+using Slang.Components;
 using System;
 
 namespace Slang.Lexer.Tokens
@@ -11,20 +12,27 @@ namespace Slang.Lexer.Tokens
     /// <summary>
     /// Represents an operator that takes one operand
     /// </summary>
-    public class UnaryOperatorFactory : IOperatorFactory
-    {        
+    public abstract class UnaryOperatorFactory : IOperatorFactory
+    {
+        /// <summary>
+        /// Represents this operator as a string
+        /// </summary>
+        public abstract string OperatorString { get; }
+
         /// <summary>
         /// Gets whether or not the operand should be evaluated
         /// </summary>
-        bool EvaluateOperand { get; }
+        public abstract bool EvaluateOperand { get; }
 
         public virtual int MatchToken(StringStream stream, out Token token)
         {
             int c = '\0';
+            token = default;
             for(int idx = 0; idx < OperatorString.Length && c != -1; ++idx) {
                 if (OperatorString[idx] != (c = stream.Read()))
                     return 0;
             }
+            token = new Token(this, OperatorString, TokenType.UnaryOperator);
             return OperatorString.Length;
         }
     }
